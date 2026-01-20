b9f7261aa41a48f69343b6fb236e04bebbf13dd5
yusuuf-dev <lucas2030garcia@gmail.com>
2026-01-20 10:13:43 +0100

    add tests to main and edit makefile

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -2,9 +2,19 @@
 
 int main()
 {
-    std::string str = "33.3698521";
-
-    
-    ScalarConverter::convert(str);
-
+    std::cout << "test 00 : a \n";    
+    ScalarConverter::convert("a");
+    std::cout << "\ntest 01 : 88 \n";    
+    ScalarConverter::convert("88");
+    std::cout << "\ntest 02 : 12.5 \n";    
+    ScalarConverter::convert("12.5");
+    std::cout << "\ntest 03 : 99888888888888888888 \n";    
+    ScalarConverter::convert("99888888888888888888");
+    std::cout << "\ntest 04 : nan \n";    
+    ScalarConverter::convert("nan");
+    std::cout << "\ntest 05 : 5.0f \n";    
+    ScalarConverter::convert("5.0f");
+    std::cout << "\ntest 06 : 76fdg \n";    
+    ScalarConverter::convert("76fdg");
+    return 0;
 }
